df098992603bc925b57c353687066bfb2a9d6b95
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-09-20 17:21:41 -0700

    alx

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent == NULL)
 		return (NULL);
-	
+
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 	{
