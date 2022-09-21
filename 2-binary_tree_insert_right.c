1881a1ea40473f0e6b4e0ce4745e083097e5bb02
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-09-20 17:26:06 -0700

    alx'

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - a function that inserts a node as the right-child of another node
+ * binary_tree_insert_right - inserts a node as the right-child of another node
  * @parent: is a pointer to the parent node of the node to create
  * @value: is the value to put in the new node
  *
