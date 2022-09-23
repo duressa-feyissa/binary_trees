f45433627022ea7e91a4bc718118b348a5bb1e57
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-09-22 19:48:29 -0700

    alx

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,24 +8,24 @@
  */
 int tree_is_perfect(const binary_tree_t *tree)
 {
-    int l = 0, r = 0;
+	int l = 0, r = 0;
 
-    if (tree->left && tree->right)
-    {
-        l = 1 + tree_is_perfect(tree->left);
-        r = 1 + tree_is_perfect(tree->right);
-        if (r == l && r != 0 && l != 0)
-            return (r);
-        return (0);
-    }
-    else if (!tree->left && !tree->right)
-    {
-        return (1);
-    }
-    else
-    {
-        return (0);
-    }
+	if (tree->left && tree->right)
+	{
+		l = 1 + tree_is_perfect(tree->left);
+		r = 1 + tree_is_perfect(tree->right);
+		if (r == l && r != 0 && l != 0)
+			return (r);
+		return (0);
+	}
+	else if (!tree->left && !tree->right)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
 
 /**
@@ -35,19 +35,19 @@ int tree_is_perfect(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    int result = 0;
+	int result = 0;
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    else
-    {
-        result = tree_is_perfect(tree);
-        if (result != 0)
-        {
-            return (1);
-        }
-        return (0);
-    }
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		result = tree_is_perfect(tree);
+		if (result != 0)
+		{
+			return (1);
+		}
+		return (0);
+	}
 }
