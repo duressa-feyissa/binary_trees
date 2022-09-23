6f95d77b2eb40a3991d0bd72ee39c0ac64b3ff60
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-09-22 19:13:25 -0700

    alx

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,18 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * max - find maximum of the two number
- * @a: integer value
- * @b: integer value
- * Return: max value
- */
-
-int max(int a, int b)
-{
-	return (a > b ? a : b);
-}
-
-/**
  * binary_tree_height - function that measures the height of a binary tree
  * @tree: is a pointer to the root node of the tree to measure the height
  * Return: If tree is NULL, your function must return 0
@@ -24,11 +12,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left)
-		x += 1 + binary_tree_height(tree->left);
-	if (tree->right)
-		y += 1 + binary_tree_height(tree->right);
-	return (max(x, y));
+	x += 1 + binary_tree_height(tree->left);
+	y += 1 + binary_tree_height(tree->right);
+	return (x > y ? x : y);
 }
 
 /**
